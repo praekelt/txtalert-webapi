0dc519ab39b234e783478c1bc22343cafb3e1eab
Daniël Keun <dkeun@retrorabbit.co.za>
2015-03-09 17:02:37 +0200

    Completed CD4 model for use in API Calls

--- TxtAlert/TxtAlert.API/Models/CD4.cs
+++ TxtAlert/TxtAlert.API/Models/CD4.cs
@@ -7,5 +7,10 @@ namespace TxtAlert.API.Models
 {
     public class CD4
     {
+        public string LAB_ID { get; set; }
+        public string FACILITY { get; set; }
+        public string CELL_NUM { get; set; }
+        public int CD4_VALUE { get; set; }
+        public DateTime ENROLMENT_DATE { get; set;
     }
 }
