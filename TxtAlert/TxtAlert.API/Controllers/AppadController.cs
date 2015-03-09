3843afe133c1babba3fb0293e8bc94662c12cba6
Daniël Keun <dkeun@retrorabbit.co.za>
2015-03-09 14:44:09 +0200

    Moved query execution and casting to function.

--- TxtAlert/TxtAlert.API/Controllers/AppadController.cs
+++ TxtAlert/TxtAlert.API/Controllers/AppadController.cs
@@ -26,26 +26,7 @@ namespace TxtAlert.API.Controllers
                 cmd = connection.CreateCommand();
                 cmd.CommandText = @"SELECT * FROM txtalertdb.p_appad";
 
-                MySqlDataAdapter adap = new MySqlDataAdapter(cmd);
-                DataSet ds = new DataSet();
-                adap.Fill(ds);
-
-                IEnumerable<Appad> patients = ds.Tables[0].AsEnumerable().Select(x => new Appad
-                {
-                    Ptd_No = x.Field<string>("Ptd_No"),
-                    Visit = x.Field<double>("Visit"),
-                    Return_date = x.Field<DateTime?>("Return_date"),
-                    Visit_date = x.Field<DateTime?>("Visit_date"),
-                    Status = x.Field<string>("Status"),
-                    Received_sms = x.Field<string>("Received_sms"),
-                    Data_Extraction = x.Field<string>("Data_Extraction"),
-                    Next_tcb = x.Field<DateTime?>("Next_tcb"),
-                    File_No = x.Field<string>("File_No"),
-                    Cellphone_number = x.Field<string>("Cellphone_number"),
-                    Facility_name = x.Field<string>("Facility_name")
-                });
-
-                return patients;
+                return ExecuteQuery(cmd);
             }
             catch (Exception)
             {
@@ -71,27 +52,13 @@ namespace TxtAlert.API.Controllers
             {
                 cmd = connection.CreateCommand();
                 cmd.CommandText = @"SELECT 
-                                        Ptd_No, 
-                                        File_No, 
-                                        Cellphone_number, 
-                                        Facility_name 
+                                        *
                                     FROM 
                                         txtalertdb.p_appad 
                                     GROUP BY 
                                         Ptd_No";
-                MySqlDataAdapter adap = new MySqlDataAdapter(cmd);
-                DataSet ds = new DataSet();
-                adap.Fill(ds);
 
-                IEnumerable<Appad> patients = ds.Tables[0].AsEnumerable().Select(x => new Appad
-                {
-                    Ptd_No = x.Field<string>("Ptd_No"),
-                    File_No = x.Field<string>("File_No"),
-                    Cellphone_number = x.Field<string>("Cellphone_number"),
-                    Facility_name = x.Field<string>("Facility_name")
-                });
-
-                return patients;
+                return ExecuteQuery(cmd);
             }
             catch (Exception)
             {
@@ -132,26 +99,7 @@ namespace TxtAlert.API.Controllers
                 cmd.Parameters.AddWithValue("@dateFrom", dateFrom);
                 cmd.Parameters.AddWithValue("@dateTo", dateTo);
 
-                MySqlDataAdapter adap = new MySqlDataAdapter(cmd);
-                DataSet ds = new DataSet();
-                adap.Fill(ds);
-
-                IEnumerable<Appad> patients = ds.Tables[0].AsEnumerable().Select(x => new Appad
-                {
-                    Ptd_No = x.Field<string>("Ptd_No"),
-                    Visit = x.Field<double>("Visit"),
-                    Return_date = x.Field<DateTime?>("Return_date"),
-                    Visit_date = x.Field<DateTime?>("Visit_date"),
-                    Status = x.Field<string>("Status"),
-                    Received_sms = x.Field<string>("Received_sms"),
-                    Data_Extraction = x.Field<string>("Data_Extraction"),
-                    Next_tcb = x.Field<DateTime?>("Next_tcb"),
-                    File_No = x.Field<string>("File_No"),
-                    Cellphone_number = x.Field<string>("Cellphone_number"),
-                    Facility_name = x.Field<string>("Facility_name")
-                });
-
-                return patients;
+                return ExecuteQuery(cmd);
             }
             catch (Exception)
             {
@@ -164,6 +112,30 @@ namespace TxtAlert.API.Controllers
                     connection.Close();
                 }
             }
+        }
+
+        private IEnumerable<Appad> ExecuteQuery(MySqlCommand cmd)
+        {
+            MySqlDataAdapter adap = new MySqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            adap.Fill(ds);
+
+            IEnumerable<Appad> results = ds.Tables[0].AsEnumerable().Select(x => new Appad
+            {
+                Ptd_No = x.Field<string>("Ptd_No"),
+                Visit = x.Field<double>("Visit"),
+                Return_date = x.Field<DateTime?>("Return_date"),
+                Visit_date = x.Field<DateTime?>("Visit_date"),
+                Status = x.Field<string>("Status"),
+                Received_sms = x.Field<string>("Received_sms"),
+                Data_Extraction = x.Field<string>("Data_Extraction"),
+                Next_tcb = x.Field<DateTime?>("Next_tcb"),
+                File_No = x.Field<string>("File_No"),
+                Cellphone_number = x.Field<string>("Cellphone_number"),
+                Facility_name = x.Field<string>("Facility_name")
+            });
+
+            return results;
         }
     }
 }
