3d83f1df588ae3f76c0651c3bdd0df9b9c4a092e
Daniël Keun <dkeun@retrorabbit.co.za>
2015-03-09 13:40:41 +0200

    Added ComingVisits API Call:
    -- Retrieves all upcoming visits within a certain date range given to it, using the "Next_tcb" field.

--- TxtAlert/TxtAlert.API/Controllers/AppadController.cs
+++ TxtAlert/TxtAlert.API/Controllers/AppadController.cs
@@ -70,7 +70,15 @@ namespace TxtAlert.API.Controllers
             try
             {
                 cmd = connection.CreateCommand();
-                cmd.CommandText = "SELECT Ptd_No, File_No, Cellphone_number, Facility_name  FROM txtalertdb.p_appad GROUP BY Ptd_No";
+                cmd.CommandText = @"SELECT 
+                                        Ptd_No, 
+                                        File_No, 
+                                        Cellphone_number, 
+                                        Facility_name 
+                                    FROM 
+                                        txtalertdb.p_appad 
+                                    GROUP BY 
+                                        Ptd_No";
                 MySqlDataAdapter adap = new MySqlDataAdapter(cmd);
                 DataSet ds = new DataSet();
                 adap.Fill(ds);
@@ -78,6 +86,66 @@ namespace TxtAlert.API.Controllers
                 IEnumerable<Appad> patients = ds.Tables[0].AsEnumerable().Select(x => new Appad
                 {
                     Ptd_No = x.Field<string>("Ptd_No"),
+                    File_No = x.Field<string>("File_No"),
+                    Cellphone_number = x.Field<string>("Cellphone_number"),
+                    Facility_name = x.Field<string>("Facility_name")
+                });
+
+                return patients;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<Appad> ComingVisits(DateTime dateFrom, DateTime dateTo)
+        {
+            MySqlConnection connection = new MySqlConnection(connString);
+            MySqlCommand cmd;
+            connection.Open();
+
+            try
+            {
+                cmd = connection.CreateCommand();
+                cmd.CommandText = @"SELECT 
+                                        * 
+                                    FROM 
+                                        txtalertdb.p_appad 
+                                    WHERE 
+                                        Next_tcb > NOW() 
+                                    AND 
+                                        Next_tcb
+                                            BETWEEN 
+                                                @dateFrom 
+                                            AND 
+                                                @dateTo";
+
+                cmd.Parameters.AddWithValue("@dateFrom", dateFrom);
+                cmd.Parameters.AddWithValue("@dateTo", dateTo);
+
+                MySqlDataAdapter adap = new MySqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adap.Fill(ds);
+
+                IEnumerable<Appad> patients = ds.Tables[0].AsEnumerable().Select(x => new Appad
+                {
+                    Ptd_No = x.Field<string>("Ptd_No"),
+                    Visit = x.Field<double>("Visit"),
+                    Return_date = x.Field<DateTime?>("Return_date"),
+                    Visit_date = x.Field<DateTime?>("Visit_date"),
+                    Status = x.Field<string>("Status"),
+                    Received_sms = x.Field<string>("Received_sms"),
+                    Data_Extraction = x.Field<string>("Data_Extraction"),
+                    Next_tcb = x.Field<DateTime?>("Next_tcb"),
                     File_No = x.Field<string>("File_No"),
                     Cellphone_number = x.Field<string>("Cellphone_number"),
                     Facility_name = x.Field<string>("Facility_name")
