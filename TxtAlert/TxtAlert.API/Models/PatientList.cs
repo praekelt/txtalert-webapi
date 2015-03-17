e02d74396e3546f003d6dd1b52ad28088281b3a4
dbreedt <dbreedt@retrorabbit.co.za>
2015-03-17 15:31:24 +0200

    Forced the response to be json only.
    Compressed the patient list to be a lot smaller than before.

---
+++ TxtAlert/TxtAlert.API/Models/PatientList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TxtAlert.API.Models
+{
+    public class PatientList
+    {
+        public string Facility_name { get; set; }
+        public IEnumerable<Patient> Patients { get; set; }
+    }
+}
